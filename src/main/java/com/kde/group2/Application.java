1a74e6b18c2448c3f63f4a89dfbac9870eb785ff
Some Aditya Mandal <somaditya432@gmail.com>
2017-12-04 19:43:25 +0000

    Revert "Basic framework for REST application created"

--- src/main/java/com/kde/group2/Application.java
+++ src/main/java/com/kde/group2/Application.java
@@ -1,35 +1,23 @@
 package com.kde.group2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.kde.group2.model.HousingModel;
 import com.kde.group2.variables.AccommodationType;
 import com.kde.group2.variables.County;
 
-@SpringBootApplication
 public class Application {
 
-	static Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String args[]) {
-		
-		SpringApplication.run(Application.class, args);
-		logger.info("Spring application initialized and started.");
-		logger.info("current dir = " + System.getProperty("user.dir"));
-		
-//		HousingModel model = new HousingModel();
-//
-//		model.getHouseholdsAndPersonsByType(AccommodationType.BEDSIT);
-//		model.getHouseholdsAndPersonsByCountyAndType(County.CORK, AccommodationType.CARAVAN_MOBILE_HOME);
-//		model.getHouseholdsByCountyAndType(County.WICKLOW, AccommodationType.FLAT_APARTMENT);
-//		model.getPersonsByCountyAndType(County.WESTMEATH, AccommodationType.NOT_STATED);
-//		model.getPersonsByCounty(County.CARLOW);
-//		model.getHouseholdsByCounty(County.CLARE);
-//		model.getPersonsByType(AccommodationType.ALL);
-//		model.getHouseholdsByType(AccommodationType.HOUSE_BUNGALOW);
-//		model.getHouseholdsGreaterThanXByType(AccommodationType.HOUSE_BUNGALOW, 50000);
+		HousingModel model = new HousingModel();
+
+		model.getHouseholdsAndPersonsByType(AccommodationType.BEDSIT);
+		model.getHouseholdsAndPersonsByCountyAndType(County.CORK, AccommodationType.CARAVAN_MOBILE_HOME);
+		model.getHouseholdsByCountyAndType(County.WICKLOW, AccommodationType.FLAT_APARTMENT);
+		model.getPersonsByCountyAndType(County.WESTMEATH, AccommodationType.NOT_STATED);
+		model.getPersonsByCounty(County.CARLOW);
+		model.getHouseholdsByCounty(County.CLARE);
+		model.getPersonsByType(AccommodationType.ALL);
+		model.getHouseholdsByType(AccommodationType.HOUSE_BUNGALOW);
+		model.getHouseholdsGreaterThanXByType(AccommodationType.HOUSE_BUNGALOW, 50000);
 	}
 }
