f0c92bac0b3583f10c05b7623eacb756c01fdcc3
someaditya <somaditya432@gmail.com>
2017-12-07 19:32:21 +0000

    Updated Queries

--- src/main/java/com/kde/group2/constants/Queries.java
+++ src/main/java/com/kde/group2/constants/Queries.java
@@ -116,4 +116,16 @@ public class Queries {
 				"?y <http://data.cso.ie/census-2011/property/households> ?households ." +
 				"FILTER (?households > %d)" +
 			"}";
+	public static String HOUSEHOLDS_LESSER_THAN_X_BY_TYPE = "SELECT ?countyResource ?county ?households ?geometry WHERE {" +
+			"?countyResource <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Concept> ." +
+			"?countyResource <http://www.w3.org/2000/01/rdf-schema#label> ?county ." +
+			"?countyResource <http://www.w3.org/2002/07/owl#sameAs> ?geoHiveCounty ." +
+			"?geoHiveCounty <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://ontologies.geohive.ie/osi#County> ." +
+			"?geoHiveCounty <http://www.opengis.net/ont/geosparql#hasGeometry> ?geo ." +
+			"?geo <http://www.opengis.net/ont/geosparql#asWKT> ?geometry ." +
+			"?y <http://purl.org/linked-data/sdmx/2009/dimension#refArea> ?countyResource ." +
+			"?y <http://data.cso.ie/census-2011/property/type-of-accommodation> <http://data.cso.ie/census-2011/classification/type-of-accommodation/%s> ." +
+			"?y <http://data.cso.ie/census-2011/property/households> ?households ." +
+			"FILTER (?households > %d)" +
+		"}";
 }
